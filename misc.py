f7ce461ee5624b006d0a4439979c904b42dc34a3
Dzidzor Boni <dzidzorboni@gmail.com>
2022-01-11 15:58:17 +0000

    Switching values of 3 variables

---
+++ misc.py
@@ -0,0 +1,38 @@
+'''
+x = 30
+y = 20
+
+print ('initial values')
+print ('x = 30')
+print ('y = 20'+'\n')
+
+z = x
+x = y
+y = z
+
+
+print ('final values')
+print ('x = ' +str(x))
+print ('y = ' +str(y))
+
+'''
+
+
+a = 10
+b = 20
+c = 30
+
+print ('initial values')
+print ('a = ' + str(a))
+print ('b = ' + str(b))
+print ('c = ' + str(c) + '\n')
+
+d = a
+a = b
+b = c
+c = d
+
+print ('final values')
+print ('a = ' +str(a))
+print ('b = ' +str(b))
+print ('c = ' +str(c))
