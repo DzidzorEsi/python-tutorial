79eb6996c993824307d5a094bee594f6ef051ae6
Dzidzor Boni <dzidzorboni@gmail.com>
2022-01-13 09:58:33 +0000

    Checking a number for prime number

--- misc.py
+++ misc.py
@@ -36,6 +36,8 @@ def invert_3_variables():
     print ('b = ' +str(b))
     print ('c = ' +str(c))
 
+    
+#Converting from degrees Fahrenheit to degrees Kelvin
 print ('Converting from degrees Fahrenheit to degrees Kelvin...')
 '''
 (32°F − 32) × 5/9 + 273.15 = 273.15K
@@ -43,8 +45,8 @@ print ('Converting from degrees Fahrenheit to degrees Kelvin...')
 2.solve left side of the equation
 3.solve right side of the equation
 4._return value
-5._print value
-'''
+5._print value '''
+
 
 def convert_to_degree_kelvin(temp_in_fahrenheit):
     degree_diff = temp_in_fahrenheit - 32
@@ -52,8 +54,23 @@ def convert_to_degree_kelvin(temp_in_fahrenheit):
 
     return degree_in_kelvin
 
-converted = convert_to_degree_kelvin(100)
-print ('200 degrees Fahrenheit is ' + str(converted) + ' in degrees Kelvin')
+
+#print ('100 degrees Fahrenheit is ' + str(converted) + ' in degrees Kelvin')
+
+def is_number_prime(number):
+    for i in range(2, number):
+        remainder = number%i
+
+        if remainder == 0:
+            return False
+
+    return True
+'''           
+prime_number = is_number_prime(11)
+print(prime_number)'''
+
+
+
 
     
     
