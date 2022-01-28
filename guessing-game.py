1838da2e3f6b47afe74a3ff7b6ec992fb44cac31
emardillu <ezekiel@farmerline.co>
2022-01-28 21:30:27 +0000

    Created new module for guessing game. Partially implemented function

---
+++ guessing-game.py
@@ -0,0 +1,35 @@
+
+
+
+# We are going to generate a random number
+# Let the user guess the number
+# We copare the numbers and let them know if they got it right or not
+
+
+# * define a function is_guess_right
+# * greet the user and welcome them to the program
+# * Display the rules of the games
+# * Prompt the user to guess a number (requires input)
+# * check if the provided input is a valid number
+# * check if the provided number is within range
+# * Generate a random number
+# * compare the generated number with the user's guess
+# * Display whether the number was guessed correctly
+
+
+def is_guess_right():
+    print('Hello there... Are you ready to play the guessing game????? ;)')
+    print('The rules are simple... Think of a number between 1 and 10.')
+    print('When prompted, type that number on to the screen\nand we will tell you if your guess was correct')
+
+    guess = input('Guess a number... And type it below\n')
+    guess = int(guess)
+    
+    if guess >= 1 and guess <= 10:
+        #TODO generate and random number and check if it's sames as the guess
+    else:
+        return False
+
+        
+    
+    
