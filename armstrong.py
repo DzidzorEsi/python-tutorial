d25dfb801ba15debda3ccfe426782c8af55d74aa
Dzidzor Boni <dzidzorboni@gmail.com>
2022-02-10 21:32:17 +0000

    Counting the number of armstrong numbers using edge cases

--- armstrong.py
+++ armstrong.py
@@ -15,6 +15,7 @@
 
 #Steps to count the number of armstrong numbers:
 #*define function count_armstrong_numbers(list):
+#*Set a restriction for input that is not a list and return#*Set a rstriction for the number 0 in the list
 #*create a variable, called count and assign to 0
 #*write a loop for the list
 #*run each iteration through the is_armstrong_number function
@@ -25,6 +26,12 @@
 
 
 def is_armstrong_number(num):
+    if type(num) != int:
+        return False
+    
+    if num == 0 or num == 1:
+        return False
+    
     num_str = str(num)
     exponent = len(num_str)
     _sum = (0)
@@ -41,22 +48,33 @@ def is_armstrong_number(num):
     
     
 def count_armstrong_numbers(_list):
-    count = (0)
+    if type(_list) == int:
+        return('List expected, but found integer')
+    elif type(_list) == str:
+        return('List expected, but found string')
+        
+    count = 0
     for i in (_list):
-        is_armstrong_number(i)
-
-    if is_armstrong_number(i) == True:
-        count += 1
-    else:
-        pass
+        ans = is_armstrong_number(i)
+        if ans == True:
+            count = count + 1
 
     return count
 
-my_list = ([153, 834, 1634, 92331, 111, 0])
+my_list = [123, 1153, 153, 'top']    
 number_count = count_armstrong_numbers(my_list)
 print(number_count)
 
-                
+
+#Steps to count the number of armstrong numbers:
+#*define function count_armstrong_numbers(list):
+#*create a variable, called count and assign to 0
+#*write a loop for the list
+#*run each iteration through the is_armstrong_number function
+#*write if condition to add count 1 for when armstrong function returns True
+#write else condition for nothing to be done when function returns false
+#*return value of count variable
+#*print statement showing the total number of armstrong numbers                
                 
     
 
